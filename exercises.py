652fc010e4764b2d50f3898ca593dbbe8b6e9e81
xiokevc <xiokevc@gmail.com>
2025-09-04 12:19:54 -0400

    exercise 4

--- exercises.py
+++ exercises.py
@@ -65,7 +65,13 @@ print('Exercise 3:', slice_foods())
 # Using the home_town dictionary, assign to a variable named home_town_message a string with this format: “I was born in <city>, <state> - population of <population>”
 
 def hometown_info():
-    # your code here
+    home_town = {
+        'city': 'Minneapolis',
+        'state': 'Minnesota',
+        'population': '428579'
+    }
+    home_town_message = f"I was born in {home_town['city']}, {home_town['state']} - population of {home_town['population']}"
+    return home_town_message
 
 # Call the function and print the result
 print('Exercise 4:', hometown_info())
